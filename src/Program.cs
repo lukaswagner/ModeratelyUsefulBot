5c7e3b895495ffcb4422cdd12b92330b5bc04377
Lukas Wagner <lukas.wagner@student.hpi.de>
2018-03-17 22:19:17 +0100

    react to message

--- src/Program.cs
+++ src/Program.cs
@@ -1,26 +1,49 @@
 ﻿using System;
 using System.IO;
 using System.Threading.Tasks;
+using Telegram.Bot;
 
 namespace moderately_useful_bot
 {
     class Program
     {
-        static void Main(string[] args)
+        private static TelegramBotClient _botClient;
+
+        private static void Main(string[] args)
         {
-            TestApiAsync().Wait();
-            Console.ReadLine();
+            _startBot().Wait();
+            Console.WriteLine("Type \"exit\" to stop the bot.");
+            var running = true;
+            while(running)
+                if(Console.ReadLine() == "exit")
+                {
+                    _botClient?.StopReceiving();
+                    running = false;
+                }
         }
 
-        static async Task TestApiAsync()
+        private static async Task _startBot()
         {
             using (StreamReader sr = new StreamReader("data/token.txt"))
             {
                 var token = sr.ReadLine();
                 Console.WriteLine("Token: " + token);
-                var botClient = new Telegram.Bot.TelegramBotClient(token);
-                var me = await botClient.GetMeAsync();
+                _botClient = new TelegramBotClient(token);
+                _botClient.OnUpdate += _onUpdate;
+                _botClient.StartReceiving();
+                var me = await _botClient.GetMeAsync();
                 Console.WriteLine("Hello! My name is " + me.FirstName);
+            }
+        }
+
+        private static void _onUpdate(object sender, Telegram.Bot.Args.UpdateEventArgs e)
+        {
+            var type = e.Update.Type;
+            if(type == Telegram.Bot.Types.Enums.UpdateType.MessageUpdate)
+            {
+                var message = e.Update.Message;
+                Console.WriteLine("Received Message: " + message.Text);
+                _botClient.SendTextMessageAsync(message.Chat.Id, "You said \"" + message.Text + "\"");
             }
         }
     }
